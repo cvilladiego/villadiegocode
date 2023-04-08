481a2bbd5441b5573a0168ee16f6e855fe48ef62
Carlos Villadiego <carlovinar93@gmail.com>
2023-04-08 09:56:58 -0500

    pagina deperfil casi terminada, faltan animaciones

--- src/componentes/about/About.jsx
+++ src/componentes/about/About.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../Navbar/Navbar";
 import styles from "./about.module.css";
-import imgPerfil from '../../assets/img/IMG_0492.jpg'
+import imgPerfil from "../../assets/img/IMG_0492.jpg";
+import { datos } from "../../data/data";
 
 function About() {
   return (
@@ -9,6 +10,15 @@ function About() {
       <section>
         <div className={styles.containerAbout}>
           <img className={styles.perfilImg} src={imgPerfil} alt="PERFIL" />
+        </div>
+      </section>
+
+      <section>
+        <div className={styles.containerPerfil}>
+          <h1 className={styles.perfilTitulo}>Carlos Villadiego</h1>
+          {datos.map(  dato => <h4 className={styles.perfilSubTitulo}>{dato.profesion}</h4>)}
+          {datos.map(  dato2 => <p>{dato2.perfilProfesional}</p>)}
+          
         </div>
       </section>
     </>
